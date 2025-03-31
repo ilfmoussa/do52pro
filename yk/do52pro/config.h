3cfb8ca947c83d479621c694867eb669ab39c240
Ilf Moussa <ilf.moussa@imodup.com>
2025-03-31 19:57:49 +0200

    add qmk firwmare

---
+++ yk/do52pro/config.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define SPLIT_USB_DETECT
+
+#define SPLIT_USB_TIMEOUT 2500 
+
+/* 		 Define Handedness 		 */
+// #define EE_HANDS
+
+/* 		 Pointing Device 		 */
+#ifdef PS2_DRIVER_BUSYWAIT
+#   define PS2_CLOCK_PIN   B1
+#   define PS2_DATA_PIN    B2
+#define PS2_MOUSE_USE_REMOTE_MODE
+#   define PS2_MOUSE_ROTATE 270
+#endif
+
+#ifdef PS2_DRIVER_INTERRUPT
+#   define PS2_MOUSE_USE_REMOTE_MODE
+#   define PS2_CLOCK_PIN   B1
+#   define PS2_DATA_PIN    B2
+
+#   define PS2_MOUSE_ROTATE 270
+#   define PS2_INT_INIT()  do {  \
+        PCMSK0 |= (1<<PCINT1);   \
+    } while (0)
+#   define PS2_INT_ON()  do {    \
+        PCICR |= (1<<PCIE0);\
+    } while (0)
+#   define PS2_INT_OFF() do {    \
+        PCICR &= ~(1<<PCIE0);    \
+    } while (0)
+#   define PS2_INT_VECT   PCINT0_vect
+#endif
+
+/* 		 Unused Functions 		 */
+// #define NO_ACTION_ONESHOT
+
+/* 		 Debug 		 */
+// #define DEBUG_MATRIX_SCAN_RATE
+// #define PS2_MOUSE_DEBUG_HID
+// #define PS2_MOUSE_DEBUG_RAW
+# define TAPPING_TERM 120
---
+++ yk/do52pro/rules.mk
@@ -0,0 +1,15 @@
+SERIAL_DRIVER = bitbang
+SWAP_HANDS_ENABLE =  no
+
+#  		 Pointing Device 		 #
+# For use with Trackpoint
+# The slave split has to be configured to not have PS2 Enabled. Otherwise, inputs will be missed.
+# Tested with interrupts but the above workaround does not work even when PS2 is disabled for slave split. Input still lags.
+PS2_MOUSE_ENABLE = yes
+PS2_ENABLE = no
+PS2_DRIVER = interrupt
+# PS2_DRIVER = busywait
+
+COMBO_ENABLE = no
+
+PS2_USE_BUSYWAIT = yes
